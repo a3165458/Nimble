a86a5b9961e3ecdf689bfa060c9dfdf26bd9a28f
smeb y <48400087+a3165458@users.noreply.github.com>
2024-03-31 15:20:47 +0800

    Update Nimble.sh

--- Nimble.sh
+++ Nimble.sh
@@ -43,7 +43,7 @@ rm -rf /usr/local/go
 cd /usr/local
 wget https://go.dev/dl/go1.22.1.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.22.1.linux-amd64.tar.gz
-echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.bashrc
+echo "export PATH=\$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bashrc
 source ~/.bashrc
 go version
 
