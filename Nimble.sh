f7d046e2f6129c49cabe0835c1dd81ece17f98ef
smeb y <48400087+a3165458@users.noreply.github.com>
2024-04-01 11:16:32 +0800

    Update Nimble.sh

--- Nimble.sh
+++ Nimble.sh
@@ -22,6 +22,10 @@ function install_node() {
     apt update
     apt install -y git python3-venv bison screen binutils gcc make bsdmainutils python3-pip
 
+
+# 安装numpy
+    pip install numpy==1.24.4
+
     # 安装GO
     rm -rf /usr/local/go
     wget https://go.dev/dl/go1.22.1.linux-amd64.tar.gz -P /tmp/
